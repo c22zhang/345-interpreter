3e2ed1ca46ffad201256e5260256ba3285b3a20c
Chris Zhang <clz16@case.edu>
2018-02-14 16:49:26 -0500

    added identifier functions for var-decl, assignment, return

--- interpreter.scm
+++ interpreter.scm
@@ -2,4 +2,25 @@
 ;Jeremy Chan
 ;EECS 345 Interpreter project, part 1
 
-load("simpleParser.scm")
+(load "simpleParser.scm")
+
+(define var-declaration?
+  (lambda (expression)
+    (cond
+      ((null? expression) #f)
+      ((eq? 'var (car expression)) #t)
+      (else #f))))
+
+(define assignment?
+  (lambda (expression)
+    (cond
+      ((null? expression) #f)
+      ((eq? '= (car expression)) #t)
+      (else #f))))
+
+(define return?
+  (lambda (expression)
+    (cond
+      ((null? expression) #f)
+      ((eq? 'return (car expression)) #t)
+      (else #f))))
