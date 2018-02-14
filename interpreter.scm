21988d5d328db12fa90d53f6fc73abaaf8c1954e
Chris Zhang <clz16@case.edu>
2018-02-14 17:37:02 -0500

    here u go jermy

--- interpreter.scm
+++ interpreter.scm
@@ -24,3 +24,43 @@
       ((null? expression) #f)
       ((eq? 'return (car expression)) #t)
       (else #f))))
+
+(define arithmetic-operator?
+  (lambda (op)
+    (cond
+      ((eq? '+ op) #t)
+      ((eq? '- op) #t)
+      ((eq? '* op) #t)
+      ((eq? '/ op) #t)
+      ((eq? '% op) #t)
+      (else #f))))
+
+(define m.value.int
+  (lambda (e)
+    (cond
+      ((number? e) e)
+      ((eq? '+ (operator e)) (+ (m.value.int (operand1 e)) (m.value.int(operand2 e))))
+      ((eq? '- (operator e)) (- (m.value.int (operand1 e)) (m.value.int(operand2 e))))
+      ((eq? '* (operator e)) (* (m.value.int (operand1 e)) (m.value.int(operand2 e))))
+      ((eq? '/ (operator e)) (quotient (m.value.int (operand1 e)) (m.value.int(operand2 e))))
+      ((eq? '% (operator e)) (remainder (m.value.int (operand1 e)) (m.value.int(operand2 e))))
+      (else (error 'badop "undefined operator")))))
+
+(define m.value.boolean
+  (lambda (e)
+    (cond
+      ((or (arithmetic-operator? e) (number? e)) (m.value.int e))
+      ((eq? '> (operator e)) (> (m.value.boolean(operand1 e)) (m.value.boolean(operand2 e))))
+      ((eq? '>= (operator e)) (>= (m.value.boolean(operand1 e)) (m.value.boolean(operand2 e))))
+      ((eq? '< (operator e)) (< (m.value.boolean(operand1 e)) (m.value.boolean(operand2 e))))
+      ((eq? '<= (operator e)) (<= (m.value.boolean(operand1 e)) (m.value.boolean(operand2 e))))
+      ((eq? '== (operator e)) (eq? (m.value.boolean(operand1 e)) (m.value.boolean(operand2 e))))
+      ((eq? '&& (operator e)) (and (m.value.boolean(operand1 e)) (m.value.boolean(operand2 e))))
+      ((eq? '|| (operator e)) (or (m.value.boolean(operand1 e)) (m.value.boolean(operand2 e))))
+      ((eq? '!= (operator e)) (not (eq? (m.value.boolean(operand1 e)) (m.value.boolean(operand2 e)))))
+      (else (error 'badop "undefined operator")))))
+
+(define operator car)
+  
+(define operand1 cadr)
+(define operand2 caddr)
