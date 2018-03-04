f08b16b513ac3a5495e6b77c23d81ed89ce6768f
Jeremy Chan <jsc126@case.edu>
2018-03-04 13:43:24 -0500

    can now add layers to state

--- interpreter.scm
+++ interpreter.scm
@@ -42,88 +42,118 @@
 	 ((while-statement? (current-expression parse-tree)) (interpret-state (next-expressions parse-tree) (M-state-while (current-expression parse-tree) state) return))
 	 (else (interpret-state (next-expressions parse-tree) state return)))))
 
+
 ;finds the return value associated with a return statement
 (define get-return-value
   (lambda (expression state)
     (if (return?  expression)
-		(m-value-expr (cadr expression) state)
-		(error 'badop "no return value"))))
+        (m-value-expr (cadr expression) state)
+        (error 'badop "no return value"))))
 
 ;returns true if an expression is a var-declaration
 (define var-declaration?
   (lambda (expression)
     (cond
-	 ((null? expression) #f)
-	 ((eq? 'var (keyword expression)) #t)
-	 (else #f))))
+      ((null? expression) #f)
+      ((eq? 'var (keyword expression)) #t)
+      (else #f))))
 
 ;returns true if an expression is an assignment statement
 (define assignment?
   (lambda (expression)
     (cond
-	 ((null? expression) #f)
-	 ((eq? '= (keyword expression)) #t)
-	 (else #f))))
+      ((null? expression) #f)
+      ((eq? '= (keyword expression)) #t)
+      (else #f))))
 
 ;returns true if an expression is a return statement
 (define return?
   (lambda (expression)
     (cond
-	 ((null? expression) #f)
-	 ((eq? 'return (keyword expression)) #t)
-	 (else #f))))
+      ((null? expression) #f)
+      ((eq? 'return (keyword expression)) #t)
+      (else #f))))
 
 ;returns true if an expression is an if statement
 (define if-statement?
   (lambda (expression)
-	(cond
-	 ((null? expression) #f)
-	 ((eq? 'if (keyword expression)) #t)
-	 (else #f))))
+    (cond
+      ((null? expression) #f)
+      ((eq? 'if (keyword expression)) #t)
+      (else #f))))
 
 ;returns true if an expression is a while statement
 (define while-statement?
   (lambda (expression)
-	(cond
-	 ((null? expression) #f)
-	 ((eq? 'while (keyword expression)) #t)
-	 (else #f))))
+    (cond
+      ((null? expression) #f)
+      ((eq? 'while (keyword expression)) #t)
+      (else #f))))
 
 ;returns true if an expression is an arithmetic operator
 (define arithmetic-operator?
   (lambda (op)
     (cond
-	 ((eq? '+ op) #t)
-	 ((eq? '- op) #t)
-	 ((eq? '* op) #t)
-	 ((eq? '/ op) #t)
-	 ((eq? '% op) #t)
-	 (else #f))))
+      ((eq? '+ op) #t)
+      ((eq? '- op) #t)
+      ((eq? '* op) #t)
+      ((eq? '/ op) #t)
+      ((eq? '% op) #t)
+      (else #f))))
 
 ;returns true if an expression is an atom
 (define atom?
   (lambda (a)
     (cond
-	 ((list? a) #f)
-	 ((null? a) #f)
-	 (else #t))))
+      ((list? a) #f)
+      ((null? a) #f)
+      (else #t))))
 
 ;returns true if the state-var-list contains var
 (define contains?
   (lambda (var state-var-list)
     (cond
-	 ((null? state-var-list) #f)
-	 ((eq? var (car state-var-list)) #t)
-	 (else (contains? var (cdr state-var-list))))))
+      ((null? state-var-list) #f)
+      ((eq? var (car state-var-list)) #t)
+      (else (contains? var (cdr state-var-list))))))
+
+(define startBlock?
+  (lambda (expr)
+	(cond
+	 ((null? expr) #f)
+	 ((eq? '= (keyword expr)) #t)
+	 (else #f))))
+					
+;stores the variable in the state
+(define store-variable-in-state
+  (lambda (var state)
+    (if (contains? var (state-vars state))
+        state
+        (append-var state var))))
+
+;;helper method for appending item to end of list
+(define append-var
+  (lambda (state var)
+    (cond
+      ((null? list) (cons val '()))
+      (else (cons (append (car state) (cons var '())) (cdr state))))))
+
+;stores the value associated with the variable in the sate
+(define store-variable-value-in-state
+  (lambda (var value state)
+    (cond
+      ((eq? getVariableValue #f)(add-value-to-variable var value state))
+      ((eq? (getVariableValue state var) value) state)
+      (else (cons (cons var (state-vars state)) (cons (append (cons (m-value-expr value state) '()) (state-vals state)) '()))))))
 
 ;;helper function for add-value-to-variable
 (define remove-variable-from-list
   (lambda (var lis)
     (cond
-	 ((null? lis) '())
-	 ((eq? (eq? (car lis) var) #f)
-	  (cons (car lis) (remove-variable-from-list var (cdr lis))))
-	 (else (cdr lis)))))
+      ((null? lis) '())
+      ((eq? (eq? (car lis) var) #f)
+       (cons (car lis) (remove-variable-from-list var (cdr lis))))
+      (else (cdr lis)))))
 
 ;;adds value to variable if it already exists but uninitialized in state
 (define add-value-to-variable
@@ -135,24 +165,43 @@
 (define getVariableValue
   (lambda (state var)
     (cond
-	 ((null? state) (error "Variable not declared"))
-	 ((null? (state-vals state)) #f) ;;variable is initialized but not declared
-	 ((eq? (car (state-vars state)) var) (car (state-vals state)))
-         ((and (list? var) (eq? (car (state-vars state)) (car var))) (car (state-vals state)))
-	 (else (getVariableValue (cons (cdr (state-vars state)) (cons (cdr (state-vals state)) '())) var)))))
+      ((null? state) (error "Variable not declared"))
+      ((null? (state-vals state)) #f) ;;variable is initialized but not declared
+      ((eq? (car (state-vars state)) var) (car (state-vals state)))
+      ((and (list? var) (eq? (car (state-vars state)) (car var))) (car (state-vals state)))
+      (else (getVariableValue (cons (cdr (state-vars state)) (cons (cdr (state-vals state)) '())) var)))))
 
 ;;returns stateVals with deleted variable value
 (define modifyStateVals
   (lambda (var stateVars stateVals)
     (cond
-	 ((null? stateVars) stateVals)
-	 ((eq? (car stateVars) var) (cdr stateVals))
-	 (else (cons (car stateVals) (modifyStateVals var (cdr stateVars) (cdr stateVals)))))))
+      ((null? stateVars) stateVals)
+      ((eq? (car stateVars) var) (cdr stateVals))
+      (else (cons (car stateVals) (modifyStateVals var (cdr stateVars) (cdr stateVals)))))))
 
 ;;helper method that allows variables to be revalued 
 (define modifyVariableValue
   (lambda (var val state)
     (cons (cons var (remove-variable-from-list var (state-vars state))) (cons (cons (m-value-expr val state) (modifyStateVals var (state-vars state) (state-vals state))) '()))))
+
+;;add layer to state
+;;param newLayer is a state in form '((a b) (1 2))
+(define addLayer
+  (lambda (newLayer state)
+    (cond
+      ((list? (car (state-vars state))) (cons (cons (state-vars newLayer) (state-vars state)) (cons (cons (state-vals newLayer) (state-vals state)) '())))
+       (else (cons (cons (state-vars newLayer) (cons (state-vars state) '()))
+		  (cons (cons (state-vals newLayer) (cons (state-vals state) '())) '()))))))
+
+;;helper function for removeTopLayer
+(define removeTopLayer-cps
+  (lambda (state return)
+    (return (cons (cdr (state-vars state)) (cons (cdr (state-vals state)) '())))))
+	
+;;removes topmost layer from state
+(define removeTopLayer
+  (lambda (state)
+	(removeTopLayer-cps state (lambda (v) v))))
 
 ;;declare variable, or initialize variable
 (define M-state-declare
@@ -194,7 +243,7 @@
 ;returns true if an atom is a variable
 (define variable?
   (lambda (varName)
-    (if (and(eq?(number? varName)#f) (eq?(boolean-operator?  varName)#f))
+    (if (and(eq?(number? varName)#f) (eq? (boolean-operator?  varName)#f))
         #t
         #f)))
 
@@ -207,54 +256,54 @@
       ((number? e) #t)
       ((and (list? e) (or (boolean-operator? (operator e)) (arithmetic-operator? (operator e)))) #t)
       (else #f))))
-     
+
 ;;assign value to variable if it exists
 (define M-state-assign
   (lambda (e state)
     (cond
-	 ((eq?(contains? (varName e) (state-vars state)) #f) (error "variable not declared")) 
-	 ((eq?(getVariableValue state (varName e))#f) (add-value-to-variable (varName e) (varValue e) state))
-	 ((eq?(eq?(getVariableValue state (varName e)) (varValue e))#f) (modifyVariableValue (varName e) (caddr e) state)))))
+      ((eq?(contains? (varName e) (state-vars state)) #f) (error "variable not declared")) 
+      ((eq?(getVariableValue state (varName e)) #f) (add-value-to-variable (varName e) (varValue e) state))
+      ((eq?(eq?(getVariableValue state (varName e)) (varValue e))#f) (modifyVariableValue (varName e) (caddr e) state)))))
 
 ;;determines proper method to call based on statement
 (define M-state-stmt
   (lambda (e state)
-	(cond
-	 ((arithmetic-operator? (car e)) (M-state-assign e state))
-	 ((var-declaration? e) (M-state-declare e state))
-	 ((assignment? e) (M-state-assign e state) )
-	 ((return? e) (get-return-value e state))
-	 ((if-statement? e) (M-state-if-else e state))
-	 ((while-statement? e) (M-state-while e state)))))
+    (cond
+      ((arithmetic-operator? (car e)) (M-state-assign e state))
+      ((var-declaration? e) (M-state-declare e state))
+      ((assignment? e) (M-state-assign e state) )
+      ((return? e) (get-return-value e state))
+      ((if-statement? e) (M-state-if-else e state))
+      ((while-statement? e) (M-state-while e state)))))
 
 ;;main if statement controller
 (define M-state-if
   (lambda (e state)
-	(if(eq?(null? (cdddr e)) #f)
-	   (if-else e state) 
-	   (if-only e state))))
+    (if(eq?(null? (cdddr e)) #f)
+       (if-else e state) 
+       (if-only e state))))
 
 ;;helper method for if else statements
 (define if-else
   (lambda (e state)
-	(if(m-value-boolean (cond-stmt e) state)
-	   (M-state-stmt (then-stmt e) state)
-	   (M-state-stmt (else-stmt e) state))))
+    (if(m-value-boolean (cond-stmt e) state)
+       (M-state-stmt (then-stmt e) state)
+       (M-state-stmt (else-stmt e) state))))
 
 
 ;;helper method for if only statements
 (define if-only
   (lambda (e state)
     (if (m-value-boolean (cond-stmt e) state)
-	(M-state-stmt (then-stmt e) state)
+        (M-state-stmt (then-stmt e) state)
         state)))
 
 ;modify the state based on the expression/condition of a while loop
 (define M-state-while
   (lambda (e state)
     (if(m-value-boolean (cond-stmt e) state)
-       (M-state-while e (M-state-stmt (then-tmt e) state))
-	   state)))
+       (M-state-while e (M-state-stmt (then-stmt e) state))
+       state)))
 
 ;returns true if an operator is a boolean operator
 (define boolean-operator?
@@ -288,47 +337,47 @@
 (define m-value-int
   (lambda (e state)
     (if (and (eq? (list? e)#f) (boolean-operator? e))
-          (if (and (variable? e) (eq?(getVariableValue state e)#f))
-              (error "Value undeclared")))
+        (if (and (variable? e) (eq? (getVariableValue state e)#f))
+            (error "Value undeclared")))
     (cond
-	 ((number? e) e)
-         ((and (atom? e) (eq? (getVariableValue state e) #f)) (error "no value for variable"))
-	 ((atom? e) (getVariableValue state e))
-	 ((number? (operator e)) (m-value-int (operator e) state))
-	 ((eq? '+ (operator e)) (+ (m-value-int (operand1 e) state) (m-value-int(operand2 e) state)))
-         ((and (eq? '- (operator e)) (null? (cddr e))) (* -1 (m-value-int (operand1 e) state)))
-	 ((eq? '- (operator e)) (- (m-value-int (operand1 e) state) (m-value-int(operand2 e) state)))
-	 ((eq? '* (operator e)) (* (m-value-int (operand1 e) state) (m-value-int(operand2 e) state)))
-	 ((eq? '/ (operator e)) (quotient (m-value-int (operand1 e) state) (m-value-int(operand2 e) state)))
-	 ((eq? '% (operator e)) (remainder (m-value-int (operand1 e) state) (m-value-int(operand2 e) state)))
-         ((atom? (operator e)) (m-value-int (operator e) state))
-	 (else (error 'badop "undefined operator")))))
+      ((number? e) e)
+      ((and (atom? e) (eq? (getVariableValue state e) #f)) (error "no value for variable"))
+      ((atom? e) (getVariableValue state e))
+      ((number? (operator e)) (m-value-int (operator e) state))
+      ((eq? '+ (operator e)) (+ (m-value-int (operand1 e) state) (m-value-int(operand2 e) state)))
+      ((and (eq? '- (operator e)) (null? (cddr e))) (* -1 (m-value-int (operand1 e) state)))
+      ((eq? '- (operator e)) (- (m-value-int (operand1 e) state) (m-value-int (operand2 e) state)))
+      ((eq? '* (operator e)) (* (m-value-int (operand1 e) state) (m-value-int (operand2 e) state)))
+      ((eq? '/ (operator e)) (quotient (m-value-int (operand1 e) state) (m-value-int (operand2 e) state)))
+      ((eq? '% (operator e)) (remainder (m-value-int (operand1 e) state) (m-value-int (operand2 e) state)))
+      ((atom? (operator e)) (m-value-int (operator e) state))
+      (else (error 'badop "undefined operator")))))
 
 ;;expression evaluator for boolean/comparison operators/expressions
 (define m-value-boolean
   (lambda (e state)
-	(cond
-	 ((or (arithmetic-operator? e) (number? e)) (m-value-int e state))
-	 ((eq? 'true e) #t)
-	 ((eq? 'false e) #f)
-	 ((atom? e) (m-value-int e state))
-	 ((or (arithmetic-operator? (operator e)) (number? (operator e))) (m-value-int e state))
-         ((eq? '! (operator e)) (not (m-value-boolean(operand1 e) state)))
-	 ((eq? '> (operator e)) (> (m-value-boolean(operand1 e) state) (m-value-boolean(operand2 e) state)))
-	 ((eq? '>= (operator e)) (>= (m-value-boolean(operand1 e) state) (m-value-boolean(operand2 e) state)))
-	 ((eq? '< (operator e)) (< (m-value-boolean(operand1 e) state) (m-value-boolean(operand2 e) state)))
-	 ((eq? '<= (operator e)) (<= (m-value-boolean(operand1 e) state) (m-value-boolean(operand2 e) state)))
-	 ((eq? '== (operator e)) (eq? (m-value-boolean(operand1 e) state) (m-value-boolean(operand2 e) state)))
-	 ((eq? '&& (operator e)) (and (m-value-boolean(operand1 e) state) (m-value-boolean(operand2 e) state)))
-	 ((eq? '|| (operator e)) (or (m-value-boolean(operand1 e) state) (m-value-boolean(operand2 e) state)))
-	 ((eq? '!= (operator e)) (not (eq? (m-value-boolean(operand1 e) state) (m-value-boolean(operand2 e) state))))
-	 (else (error 'badop "undefined operator")))))
+    (cond
+      ((or (arithmetic-operator? e) (number? e)) (m-value-int e state))
+      ((eq? 'true e) #t)
+      ((eq? 'false e) #f)
+      ((atom? e) (m-value-int e state))
+      ((or (arithmetic-operator? (operator e)) (number? (operator e))) (m-value-int e state))
+      ((eq? '! (operator e)) (not (m-value-boolean (operand1 e) state)))
+      ((eq? '> (operator e)) (> (m-value-boolean (operand1 e) state) (m-value-boolean (operand2 e) state)))
+      ((eq? '>= (operator e)) (>= (m-value-boolean (operand1 e) state) (m-value-boolean (operand2 e) state)))
+      ((eq? '< (operator e)) (< (m-value-boolean (operand1 e) state) (m-value-boolean (operand2 e) state)))
+      ((eq? '<= (operator e)) (<= (m-value-boolean (operand1 e) state) (m-value-boolean (operand2 e) state)))
+      ((eq? '== (operator e)) (eq? (m-value-boolean (operand1 e) state) (m-value-boolean (operand2 e) state)))
+      ((eq? '&& (operator e)) (and (m-value-boolean (operand1 e) state) (m-value-boolean (operand2 e) state)))
+      ((eq? '|| (operator e)) (or (m-value-boolean (operand1 e) state) (m-value-boolean (operand2 e) state)))
+      ((eq? '!= (operator e)) (not (eq? (m-value-boolean (operand1 e) state) (m-value-boolean (operand2 e) state))))
+      (else (error 'badop "undefined operator")))))
 
 ;true/false wrapper for #t/#f
 (define boolean-wrapper
   (lambda (bool)
     (cond
-	 ((eq? #t bool) 'true)
-	 ((eq? #f bool) 'false)
-	 (else error "expected boolean"))))
+      ((eq? #t bool) 'true)
+      ((eq? #f bool) 'false)
+      (else error "expected boolean"))))
 
